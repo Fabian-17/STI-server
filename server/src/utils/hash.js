43f6fa9a3415b96176e9d08025817521fb0b93ac
Fabian Villagra <fabianvillagra28@gmail.com>
2024-05-05 02:26:54 -0300

    feat: archivo de servicios del usuario, archivo para encontrar usuarios por su ID, función para comparar contraseñas

--- server/src/utils/hash.js
+++ server/src/utils/hash.js
@@ -4,3 +4,8 @@ export const hashPassword = async (text) => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(text, salt);
 };
+
+export async function comparePassword(plainPassword, hashedPassword) {
+    const isPasswordValid = await bcrypt.compare(plainPassword, hashedPassword);
+    return isPasswordValid;
+}
