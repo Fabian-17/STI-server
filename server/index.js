55d27b4e78a86ae8a89bb62e7f7fe669b5f35cf6
Fabian Villagra <fabianvillagra28@gmail.com>
2024-05-05 14:26:27 -0300

    fix: configuración del servidor

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { environments } from './src/config/environments.js';
+import { connectDB } from './src/config/db.js';
+import { userRouter } from './src/routes/userRouter.js';
 
 
 const app = express();
@@ -8,6 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
+app.use('/users', userRouter);
+
+app.listen(environments.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
